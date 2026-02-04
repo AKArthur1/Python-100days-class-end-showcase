fd0d1868fcdf83e4dd1f1551df8b53da39afc772
agyeiarthur <agyeiarthur@gmail.com>
2026-02-03 16:25:02 -0800

    first

---
+++ python modules sketchpad.py
@@ -0,0 +1,179 @@
+# Python100 days class end showcase project
+
+
+ABC = "abc"
+NUMBER = 0
+
+
+
+
+
+
+
+# Section 1 - Built in functions
+
+Absolute_value = "Absolute Value: returns the absolute value of a number.  abs().  "
+# abs()
+print(f"{Absolute_value}{abs(49.3)}")
+
+
+All = "All: Returns True if all items in an iterable object are true.  all().  "
+# all()
+mylist = [True, True, True]
+x = all(mylist)
+
+# Any
+# Returns True if any item in an iterable object is true
+mylist = [False, True, False]
+any()
+x = any(mylist)
+
+# Ascii
+# Returns a readable version of an object. Replaces none-ascii characters with escape character
+# replaces foreign to English characters
+ascii()
+x = ascii("My name is Ståle")
+
+# Binary converter
+# Returns the binary version of a number
+bin()
+x = bin(37)
+print(x)
+# The result will always have the prefix 0b
+
+# Boolean Checker
+bool()
+x = bool(1)
+y  = bool(0)
+print(x)
+print(y)
+
+# Byte Array converter
+# Returns an array of bytes
+bytearray()
+x = bytearray(8)
+print(x)
+
+
+bytes()
+
+callable()
+
+chr()
+
+classmethod()
+
+compile()
+
+complex()
+
+delattr()
+
+dict()
+
+dir()
+
+divmod()
+
+enumerate()
+
+eval()
+
+exec()
+
+filter()
+
+float()
+
+format()
+
+frozenset()
+
+getattr()
+
+globals()
+
+hasattr()
+
+hash()
+
+help()
+
+hex()
+
+id()
+
+input()
+
+int()
+
+isinstance()
+
+issubclass()
+
+iter()
+
+len()
+
+list()
+
+locals()
+
+map()
+
+max()
+
+memoryview()
+
+min()
+
+next()
+
+object()
+
+oct()
+
+open()
+
+ord()
+
+pow()
+
+print()
+
+property()
+
+range()
+
+repr()
+
+reversed()
+
+round()
+
+set()
+
+setattr()
+
+slice()
+
+sorted()
+
+staticmethod()
+
+str()
+
+sum()
+
+super()
+
+tuple()
+
+type()
+
+vars()
+
+zip()
+
+
+
