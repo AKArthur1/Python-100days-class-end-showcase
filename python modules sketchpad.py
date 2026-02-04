fac48e0c19324436d5df6b32df3bd624dcfcc0b6
agyeiarthur <agyeiarthur@gmail.com>
2026-02-03 23:16:18 -0800

    feb 3rd Night end push. added print to all method sections to start applying proper code formatting

--- python modules sketchpad.py
+++ python modules sketchpad.py
@@ -19,161 +19,211 @@ print(f"{Absolute_value}{abs(49.3)}")
 
 All = "All: Returns True if all items in an iterable object are true.  all().  "
 # all()
-mylist = [True, True, True]
-x = all(mylist)
+mylist_ALL = [True, True, True]
+print(f"{All}{all(mylist_ALL)}")
 
-# Any
-# Returns True if any item in an iterable object is true
-mylist = [False, True, False]
-any()
-x = any(mylist)
+Any = "ANY: Returns True if any item in an iterable object is true"
+mylist_ANY = [False, True, False]
+# any()
+print(f"{Any}{any(mylist_ANY)}")
 
-# Ascii
-# Returns a readable version of an object. Replaces none-ascii characters with escape character
-# replaces foreign to English characters
-ascii()
-x = ascii("My name is Ståle")
+Ascii = "ASCII: Returns a readable version of an object. Replaces none-ascii characters with escape character replaces foreign to English characters"
+# ascii()
+print(f"{Ascii}{ascii("My name is Ståle")}")
 
-# Binary converter
-# Returns the binary version of a number
-bin()
-x = bin(37)
-print(x)
-# The result will always have the prefix 0b
+Binary_converter = "Binary Converter: Returns the binary version of a number. The result will always have the prefix 0b"
+# bin()
+print(f"{Binary_converter}{bin(37)}")
 
-# Boolean Checker
-bool()
-x = bool(1)
-y  = bool(0)
-print(x)
-print(y)
-
-# Byte Array converter
-# Returns an array of bytes
-bytearray()
-x = bytearray(8)
-print(x)
+Boolean_Checker = "Boolean Checker: Returns the boolean version of a specified object."
+# bool()
+print(f"{Boolean_Checker}{bool(1)}, {bool(0)}")
 
 
-bytes()
+Byte_Array_converter = "Byte Array Converter: Returns an array of bytes"
+# bytearray()
+print(f"{Byte_Array_converter}{bytearray(67)}")
+
+Bytes_Converter = "Bytes Converter: Converts objects into bytes objects, or create empty bytes object of the specified size. The difference between bytes() and bytearray() is that bytes() returns an object that cannot be modified, and bytearray() returns an object that can be modified."
+# bytes()
+print(f"{}{bytes(4)}")
 
 callable()
+print(f"{}{}")
 
 chr()
+print(f"{}{}")
 
 classmethod()
+print(f"{}{}")
 
 compile()
+print(f"{}{}")
 
 complex()
+print(f"{}{}")
 
 delattr()
+print(f"{}{}")
 
 dict()
+print(f"{}{}")
 
 dir()
+print(f"{}{}")
 
 divmod()
+print(f"{}{}")
 
 enumerate()
+print(f"{}{}")
 
 eval()
+print(f"{}{}")
 
 exec()
+print(f"{}{}")
 
 filter()
+print(f"{}{}")
 
 float()
+print(f"{}{}")
 
 format()
+print(f"{}{}")
 
 frozenset()
+print(f"{}{}")
 
 getattr()
+print(f"{}{}")
 
 globals()
+print(f"{}{}")
 
 hasattr()
+print(f"{}{}")
 
 hash()
+print(f"{}{}")
 
 help()
+print(f"{}{}")
 
 hex()
+print(f"{}{}")
 
 id()
+print(f"{}{}")
 
 input()
+print(f"{}{}")
 
 int()
+print(f"{}{}")
 
 isinstance()
+print(f"{}{}")
 
 issubclass()
+print(f"{}{}")
 
 iter()
+print(f"{}{}")
 
 len()
+print(f"{}{}")
 
 list()
+print(f"{}{}")
 
 locals()
+print(f"{}{}")
 
 map()
+print(f"{}{}")
 
 max()
+print(f"{}{}")
 
 memoryview()
+print(f"{}{}")
 
 min()
+print(f"{}{}")
 
 next()
+print(f"{}{}")
 
 object()
+print(f"{}{}")
 
 oct()
+print(f"{}{}")
 
 open()
+print(f"{}{}")
 
 ord()
+print(f"{}{}")
 
 pow()
+print(f"{}{}")
 
 print()
+print(f"{}{}")
 
 property()
+print(f"{}{}")
 
 range()
+print(f"{}{}")
 
 repr()
+print(f"{}{}")
 
 reversed()
+print(f"{}{}")
 
 round()
+print(f"{}{}")
 
 set()
+print(f"{}{}")
 
 setattr()
+print(f"{}{}")
 
 slice()
+print(f"{}{}")
 
 sorted()
+print(f"{}{}")
 
 staticmethod()
+print(f"{}{}")
 
 str()
+print(f"{}{}")
 
 sum()
+print(f"{}{}")
 
 super()
+print(f"{}{}")
 
 tuple()
+print(f"{}{}")
 
 type()
+print(f"{}{}")
 
 vars()
+print(f"{}{}")
 
 zip()
+print(f"{}{}")
 
 
 
