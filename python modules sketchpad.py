46866cc7d17983b8c831b71fafbede27fea8337a
agyeiarthur <agyeiarthur@gmail.com>
2026-02-04 19:02:07 -0800

    feb 4th Night mid push. improved code formatting

--- python modules sketchpad.py
+++ python modules sketchpad.py
@@ -14,216 +14,224 @@ NUMBER = 0
 
 Absolute_value = "Absolute Value: returns the absolute value of a number.  abs().  "
 # abs()
-print(f"{Absolute_value}{abs(49.3)}")
+print(f"{Absolute_value},\n      abs(49.3) = {abs(49.3)}")
 
 
 All = "All: Returns True if all items in an iterable object are true.  all().  "
 # all()
 mylist_ALL = [True, True, True]
-print(f"{All}{all(mylist_ALL)}")
+print(f"{All},\n      mylist_ALL = [True, True, True]   all(mylist_ALL) = {all(mylist_ALL)}")
 
 Any = "ANY: Returns True if any item in an iterable object is true"
 mylist_ANY = [False, True, False]
 # any()
-print(f"{Any}{any(mylist_ANY)}")
+print(f"{Any},\n      mylist_ANY = [False, True, False]   any(mylist_ANY) = {any(mylist_ANY)}")
 
 Ascii = "ASCII: Returns a readable version of an object. Replaces none-ascii characters with escape character replaces foreign to English characters"
 # ascii()
-print(f"{Ascii}{ascii("My name is Ståle")}")
+print(f"{Ascii},\n      ascii('My name is Ståle') = {ascii('My name is Ståle')}")
 
 Binary_converter = "Binary Converter: Returns the binary version of a number. The result will always have the prefix 0b"
 # bin()
-print(f"{Binary_converter}{bin(37)}")
+print(f"{Binary_converter},\n      bin(37) = {bin(37)}")
 
 Boolean_Checker = "Boolean Checker: Returns the boolean version of a specified object."
 # bool()
-print(f"{Boolean_Checker}{bool(1)}, {bool(0)}")
+print(f"{Boolean_Checker}{bool(1)},\n      bool(0) = {bool(0)}")
 
 
 Byte_Array_converter = "Byte Array Converter: Returns an array of bytes"
 # bytearray()
-print(f"{Byte_Array_converter}{bytearray(67)}")
+print(f"{Byte_Array_converter},\n      bytearray(4) = {bytearray(4)}")
 
 Bytes_Converter = "Bytes Converter: Converts objects into bytes objects, or create empty bytes object of the specified size. The difference between bytes() and bytearray() is that bytes() returns an object that cannot be modified, and bytearray() returns an object that can be modified."
 # bytes()
-print(f"{}{bytes(4)}")
+print(f"{Bytes_Converter},\n      bytes(4) = {bytes(4)}")
 
-callable()
-print(f"{}{}")
+Callable = "Callable: The callable() function returns True if the specified object is callable, otherwise it returns False. Functions are callable vs Variables that are not"
+# callable()
+def callable_function_example():
+    a = 5
+callable_example_2 = 5
+print(f"{Callable},\n      function def callable_function_example(): a = 5    = {callable(callable_function_example)}")
+print(f"      callable_example_2 = 5      = {callable(callable_example_2)}")
 
-chr()
-print(f"{}{}")
+Character_Unicoder = "Character Unicoder: The chr() function returns the character that represents the specified unicode"
+# chr()
+print(f"{Character_Unicoder},\n      chr(67) = {chr(67)}")
+print(f"      chr(167) = {chr(167)}")
 
-classmethod()
-print(f"{}{}")
-
-compile()
-print(f"{}{}")
-
-complex()
-print(f"{}{}")
-
-delattr()
-print(f"{}{}")
-
-dict()
-print(f"{}{}")
-
-dir()
-print(f"{}{}")
-
-divmod()
-print(f"{}{}")
-
-enumerate()
-print(f"{}{}")
-
-eval()
-print(f"{}{}")
-
-exec()
-print(f"{}{}")
-
-filter()
-print(f"{}{}")
-
-float()
-print(f"{}{}")
-
-format()
-print(f"{}{}")
-
-frozenset()
-print(f"{}{}")
-
-getattr()
-print(f"{}{}")
-
-globals()
-print(f"{}{}")
-
-hasattr()
-print(f"{}{}")
-
-hash()
-print(f"{}{}")
-
-help()
-print(f"{}{}")
-
-hex()
-print(f"{}{}")
-
-id()
-print(f"{}{}")
-
-input()
-print(f"{}{}")
-
-int()
-print(f"{}{}")
-
-isinstance()
-print(f"{}{}")
-
-issubclass()
-print(f"{}{}")
-
-iter()
-print(f"{}{}")
-
-len()
-print(f"{}{}")
-
-list()
-print(f"{}{}")
-
-locals()
-print(f"{}{}")
-
-map()
-print(f"{}{}")
-
-max()
-print(f"{}{}")
-
-memoryview()
-print(f"{}{}")
-
-min()
-print(f"{}{}")
-
-next()
-print(f"{}{}")
-
-object()
-print(f"{}{}")
-
-oct()
-print(f"{}{}")
-
-open()
-print(f"{}{}")
-
-ord()
-print(f"{}{}")
-
-pow()
-print(f"{}{}")
-
-print()
-print(f"{}{}")
-
-property()
-print(f"{}{}")
-
-range()
-print(f"{}{}")
-
-repr()
-print(f"{}{}")
-
-reversed()
-print(f"{}{}")
-
-round()
-print(f"{}{}")
-
-set()
-print(f"{}{}")
-
-setattr()
-print(f"{}{}")
-
-slice()
-print(f"{}{}")
-
-sorted()
-print(f"{}{}")
-
-staticmethod()
-print(f"{}{}")
-
-str()
-print(f"{}{}")
-
-sum()
-print(f"{}{}")
-
-super()
-print(f"{}{}")
-
-tuple()
-print(f"{}{}")
-
-type()
-print(f"{}{}")
-
-vars()
-print(f"{}{}")
-
-zip()
-print(f"{}{}")
-
-
-
+ClassMethod_Converter = "Converts a method into a class method"
+# classmethod()
+# print(f"{},\n      {}")
+#
+# compile()
+# print(f"{},\n      {}")
+#
+# complex()
+# print(f"{},\n      {}")
+#
+# delattr()
+# print(f"{},\n      {}")
+#
+# dict()
+# print(f"{},\n      {}")
+#
+# dir()
+# print(f"{},\n      {}")
+#
+# divmod()
+# print(f"{},\n      {}")
+#
+# enumerate()
+# print(f"{},\n      {}")
+#
+# eval()
+# print(f"{},\n      {}")
+#
+# exec()
+# print(f"{},\n      {}")
+#
+# filter()
+# print(f"{},\n      {}")
+#
+# float()
+# print(f"{},\n      {}")
+#
+# format()
+# print(f"{},\n      {}")
+#
+# frozenset()
+# print(f"{},\n      {}")
+#
+# getattr()
+# print(f"{},\n      {}")
+#
+# globals()
+# print(f"{},\n      {}")
+#
+# hasattr()
+# print(f"{},\n      {}")
+#
+# hash()
+# print(f"{},\n      {}")
+#
+# help()
+# print(f"{},\n      {}")
+#
+# hex()
+# print(f"{},\n      {}")
+#
+# id()
+# print(f"{},\n      {}")
+#
+# input()
+# print(f"{},\n      {}")
+#
+# int()
+# print(f"{},\n      {}")
+#
+# isinstance()
+# print(f"{},\n      {}")
+#
+# issubclass()
+# print(f"{},\n      {}")
+#
+# iter()
+# print(f"{},\n      {}")
+#
+# len()
+# print(f"{},\n      {}")
+#
+# list()
+# print(f"{},\n      {}")
+#
+# locals()
+# print(f"{},\n      {}")
+#
+# map()
+# print(f"{},\n      {}")
+#
+# max()
+# print(f"{},\n      {}")
+#
+# memoryview()
+# print(f"{},\n      {}")
+#
+# min()
+# print(f"{},\n      {}")
+#
+# next()
+# print(f"{},\n      {}")
+#
+# object()
+# print(f"{},\n      {}")
+#
+# oct()
+# print(f"{},\n      {}")
+#
+# open()
+# print(f"{},\n      {}")
+#
+# ord()
+# print(f"{},\n      {}")
+#
+# pow()
+# print(f"{},\n      {}")
+#
+# print()
+# print(f"{},\n      {}")
+#
+# property()
+# print(f"{},\n      {}")
+#
+# range()
+# print(f"{},\n      {}")
+#
+# repr()
+# print(f"{},\n      {}")
+#
+# reversed()
+# print(f"{},\n      {}")
+#
+# round()
+# print(f"{},\n      {}")
+#
+# set()
+# print(f"{},\n      {}")
+#
+# setattr()
+# print(f"{},\n      {}")
+#
+# slice()
+# print(f"{},\n      {}")
+#
+# sorted()
+# print(f"{},\n      {}")
+#
+# staticmethod()
+# print(f"{},\n      {}")
+#
+# str()
+# print(f"{},\n      {}")
+#
+# sum()
+# print(f"{},\n      {}")
+#
+# super()
+# print(f"{},\n      {}")
+#
+# tuple()
+# print(f"{},\n      {}")
+#
+# type()
+# print(f"{},\n      {}")
+#
+# vars()
+# print(f"{},\n      {}")
+#
+# zip()
+# print(f"{},\n      {}")
+#
+#
+#
