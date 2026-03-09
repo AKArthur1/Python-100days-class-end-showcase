014d71509fce32650c20abf6a78254a408afab0f
agyeiarthur <agyeiarthur@gmail.com>
2026-03-09 12:14:14 -0400

    March 9th AM push.    Started Project 02 Webscrapper Red Camera

---
+++ Project_02_Webscrapper_01_RedCameraWebData/PythonShowcase_Project_02_Webscrapper_01_RedCameraWebData .py
@@ -0,0 +1,24 @@
+### Project 02 - Webscrapper to CSV 01 - Red Camera Tech Specs ###
+
+import requests
+import pandas as pd
+
+
+### functions into separate files
+
+    # csv creation file
+    # api requests file that pulls from main code file website execution
+    # csv writing and data processing
+    # csv product comparison csv collection breaking out every row into it's own csv that updates everytime it's run to stay current on products
+    # modern data scientist common functions/calculations to be run to showcase data. ex make list from interviews and youtube day in my life and google lists on most common
+    # modern Data viz library that passes through matplot etc and outputs a cleaner data vis window
+            #should this be webapp or pop up window?
+
+# keep this main file clean and clear
+
+
+### BLANK ### ---------------------------------------------------------------------------------------------
+
+
+
+
